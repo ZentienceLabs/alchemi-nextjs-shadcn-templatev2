80481a183f23cdd9a417c9bf2656f4136d39b234
ashok-zen <117160208+ashok-zen@users.noreply.github.com>
2025-06-13 15:08:36 +0530

    remove the date fns issues

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  remotePatterns: [
+    {
+      protocol: 'https',
+      hostname: 'images.pexels.com',
+      pathname: '/images/**',
+    },
+  ],
 };
 
 export default nextConfig;
